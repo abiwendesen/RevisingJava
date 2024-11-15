6a54fda38e26cc60eeb5bc9989455bd9f059f529
abi <abiwendesen21@gmail.com>
2024-11-15 21:29:21 +0300

    I made it to look for the three stop codon

--- src/forTest.java
+++ src/forTest.java
@@ -4,9 +4,19 @@ public class forTest {
        if(firstIndex == -1){
            return "";
        }
-       System.out.println("just checking");
 
+        int taaIndex = findStopCodon(dna, firstIndex, "TAA");
+        int tagIndex = findStopCodon(dna, firstIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, firstIndex, "TGA");
+
+        int temp = Math.min(taaIndex, tagIndex);
+        int minIndex = Math.min(temp, tgaIndex);
+        if(minIndex == dna.length()){
         return "";
+
+        }
+
+        return dna.substring(firstIndex,minIndex+3);
     }
     public static int findStopCodon(String dna,int startIndex,String stopCodon){
         int currIndex = dna.indexOf(stopCodon,startIndex+3);
@@ -24,7 +34,7 @@ public class forTest {
 
     public static void main (String[] args){
 
-        String dna = "ATGACGCTAAGATAA";
+        String dna = "ATGACGCTAAGATAATGATAG";
         System.out.println(findGeen(dna));
     }
 }
