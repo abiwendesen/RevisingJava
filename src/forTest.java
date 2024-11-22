e3965b1bed2f1201c1102bf8df6ccaba5b15b13c
abiwendesen <abiwendesen21@gmail.com>
2024-11-22 22:19:55 +0300

    Added StorageResource class

--- src/forTest.java
+++ src/forTest.java
@@ -1,3 +1,4 @@
+import edu.duke.StorageResource;
 public class forTest {
     public static  String findGene(String dna, int where){
         int firstIndex = dna.indexOf("ATG", where);
@@ -32,19 +33,21 @@ public class forTest {
 
         return dna.substring(firstIndex,minIndex+3);
     }
-    public static void printAllGene(String dna){
+    public static StorageResource printAllGene(String dna){
         int startIndex = 0;
+        StorageResource geneList = new StorageResource();
+
         while(true){
 
             String currGeen = findGene(dna,startIndex);
             if(currGeen.isEmpty()){
                 break;
             }
-            System.out.println("The Gene found: "+ currGeen);
+             geneList.add(currGeen);
             startIndex = dna.indexOf(currGeen,startIndex) + currGeen.length();
 
         }
-
+      return geneList;
     }
     public static int findStopCodon(String dna,int startIndex,String stopCodon){
         int currIndex = dna.indexOf(stopCodon,startIndex+3);
@@ -60,11 +63,20 @@ public class forTest {
    return -1;
     }
 
+    public static void test(String dna){
+        System.out.println("Testing");
+        StorageResource gene = printAllGene(dna);
+        for(String s: gene.data()){
+            System.out.println(s);
+        }
+    }
+
     public static void main (String[] args){
 
        // String dna = "ATGACGCTAAGATAGTGATAG";
          String dna = "ATGATCTAATTTATGCTGCAACGGTGAAGA";
-         printAllGene(dna);
+         //printAllGene(dna);
+        test(dna);
 
     }
 }
