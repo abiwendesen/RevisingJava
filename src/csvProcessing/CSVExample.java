42272a16e3b9aeba2f940af2c0eed291862e4d47
abiwendesen <abiwendesen21@gmail.com>
2024-12-03 20:43:48 +0300

    Added try catch statement

--- src/csvProcessing/CSVExample.java
+++ src/csvProcessing/CSVExample.java
@@ -4,13 +4,21 @@ import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.csv.CSVParser;
 public class CSVExample {
     public static void realFood(){
-        FileResource fr = new FileResource();
-        CSVParser csv = fr.getCSVParser();
-        for(CSVRecord record:csv){
-            System.out.print("Name" +"\t");
-            System.out.print(record.get("Name")+"\t");
-            System.out.print(record.get("Favorite Color")+"\t");
-            System.out.println(record.get("Favorite Food"));
+        try {
+            FileResource fr = new FileResource();
+            CSVParser csv = fr.getCSVParser();
+            System.out.print("Name" + "\t");
+            System.out.print("Favorite Color" + "\t");
+            System.out.println("Favorite Food " + "\t");
+            for (CSVRecord record : csv) {
+                System.out.print(record.get("Name") + "\t");
+                System.out.print("\t");
+                System.out.print(record.get("Favorite Color") + "\t");
+                System.out.println(record.get("Favorite Food"));
+            }
+        }
+        catch(Exception e){
+           System.out.println(e) ;
         }
     }
     public static void main(String[] args){
